ccf5d24654a75c282c2a493952c1928c7d540096
oshiro-kazuki <ka.oo1213mi@gmail.com>
2021-12-30 16:05:15 +0900

    スワイプ機能追加

--- public/js/component/contents/news_slider.js
+++ public/js/component/contents/news_slider.js
@@ -1,7 +1,8 @@
 'use strict';
 // タイトル20文字・テキスト70文字　後に...
 
-const news_items = document.querySelectorAll('.news_item');
+const news_items = get_tag_byId('news_items');
+const news_item = document.querySelectorAll('.news_item');
 const news_next_btn = get_tag_byId('news_next_btn');
 const news_dot_btns = get_tag_byId('news_dot_btns');
 const news_prev_btn = get_tag_byId('news_prev_btn');
@@ -11,12 +12,12 @@ const item_view = 1;
 const item_x = [];
 
 // パネル初期表示
-if(news_items.length > 0){
-    news_items.forEach((item, index) => {
-        if(index < news_items.length - 2){
+if(news_item.length > 0){
+    news_item.forEach((item, index) => {
+        if(index < news_item.length - 2){
             item_x.push(start_news + news_width * index);
         } else {
-            item_x.push((start_news - news_width) - (news_width * ((news_items.length - 1) - index)));
+            item_x.push((start_news - news_width) - (news_width * ((news_item.length - 1) - index)));
         }
         item.style.left = `${item_x[index]}px`;
         create_tag('span', news_dot_btns, false, 'news_dot_btn');
@@ -42,7 +43,7 @@ function dot_btn_callback(item, object){
 
 const dot_btn_observer = new IntersectionObserver(dot_btn_callback, dot_btn_options);
 
-news_items.forEach(item => {
+news_item.forEach(item => {
     dot_btn_observer.observe(item)
 });
 
@@ -76,20 +77,20 @@ function news_prev_move(item, index){
 
 // ドットボタン押下スライド処理
 function news_dot_click_move(click_index){
-    news_items.forEach((item, index) => {
+    news_item.forEach((item, index) => {
         cla_remove(item, 'move');
         const click_index_diff = index - click_index;
-        const news_items_half = news_items.length / 2;
+        const news_items_half = news_item.length / 2;
         item.style.opacity = 1;
         if(index === click_index) {
             item_x[index] = start_news;
             currnt_btn(index);
         }else{
             if(click_index_diff > news_items_half){
-                item_x[index] = start_news + (news_width * (click_index_diff - news_items.length));
+                item_x[index] = start_news + (news_width * (click_index_diff - news_item.length));
             }
             else if(click_index_diff < news_items_half * -1){
-                item_x[index] = start_news + (news_width * (click_index_diff + news_items.length));
+                item_x[index] = start_news + (news_width * (click_index_diff + news_item.length));
             }else{
                 item_x[index] = start_news + news_width * click_index_diff;
             }
@@ -114,14 +115,14 @@ function currnt_btn(current_index){
 
 // 次へボタン押下
 news_next_btn.addEventListener('click', () => {
-    news_items.forEach((item, index) => {
+    news_item.forEach((item, index) => {
         news_next_move(item, index);
     });
 });
 
 // 前へボタン押下
 news_prev_btn.addEventListener('click', () => {
-    news_items.forEach((item, index) => {
+    news_item.forEach((item, index) => {
         news_prev_move(item, index);
     });
 });
@@ -132,3 +133,32 @@ news_dot_btn.forEach((btn, index) => {
         news_dot_click_move(index);
     });
 });
+
+// スワイプイベント
+let startX = null;
+let endX = null;
+
+function logSwipeStart(event) {
+	event.preventDefault();
+	startX = event.touches[0].pageX;
+}
+
+function logSwipe(event) {
+	event.preventDefault();
+	endX = event.touches[0].pageX;
+}
+
+function logSwipeEnd(event) {
+	event.preventDefault();
+    const swipe_x = endX - startX;
+
+    news_item.forEach((item, index) => {
+        0 < swipe_x ? news_prev_move(item, index) : news_next_move(item, index);
+    });
+}
+
+news_items.addEventListener('touchstart', logSwipeStart);
+
+news_items.addEventListener('touchend', logSwipeEnd);
+
+news_items.addEventListener('touchmove', logSwipe);
