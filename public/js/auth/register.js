b7a2cc006ad5c7b8e056fb45fc0eab6c54d63d2f
oshiro-kazuki <ka.oo1213mi@gmail.com>
2022-02-10 12:26:07 +0900

    エラー後のボタンと入力値制御

--- public/js/auth/register.js
+++ public/js/auth/register.js
@@ -3,14 +3,16 @@
 const submit_flg = [true,true,true,true,true,true,false,false];
 const max_len = 191;
 
-requireText('input_name', 'form_name_error', 'conf_name', max_len, submit_flg, 0, 'register_input_conf_btn', 'submit_btn');
-requireFullWidth('input_address', 'form_address_error', 'conf_address', max_len, submit_flg, 1, 'register_input_conf_btn', 'submit_btn');
-requireTell('input_tel', 'form_tel_error', 'conf_tel', submit_flg, 2, 'register_input_conf_btn', 'submit_btn');
-requireEmail('input_email', 'form_email_error', 'conf_email', max_len, submit_flg, 3, 'register_input_conf_btn', 'submit_btn');
-requirePassword('input_password', 'form_password_error', submit_flg, 4, 'register_input_conf_btn', 'submit_btn');
-requirePassword('password_confirmation', 'form_password_conf_error', submit_flg, 5, 'register_input_conf_btn', 'submit_btn');
-nullableText('input_profile', 'form_profile_error', 'conf_profile', 140, submit_flg, 6, 'register_input_conf_btn', 'submit_btn');
-nullableImage('input_profile_image', 'form_profile_image_error', 'conf_image', submit_flg, 7, 'register_input_conf_btn', 'submit_btn');
-
-sectionChange('register_input', 'register_conf', 'register_input_conf_btn');
-returnClick('register_input' ,'register_conf', 'form_return_btn');
+window.addEventListener('load', () => {
+    requireText('input_name', 'form_name_error', 'conf_name', max_len, submit_flg, 0, 'register_input_conf_btn', 'submit_btn');
+    requireFullWidth('input_address', 'form_address_error', 'conf_address', max_len, submit_flg, 1, 'register_input_conf_btn', 'submit_btn');
+    requireTell('input_tel', 'form_tel_error', 'conf_tel', submit_flg, 2, 'register_input_conf_btn', 'submit_btn');
+    requireEmail('input_email', 'form_email_error', 'conf_email', max_len, submit_flg, 3, 'register_input_conf_btn', 'submit_btn');
+    requirePassword('input_password', 'form_password_error', submit_flg, 4, 'register_input_conf_btn', 'submit_btn');
+    requirePassword('password_confirmation', 'form_password_conf_error', submit_flg, 5, 'register_input_conf_btn', 'submit_btn');
+    nullableText('input_profile', 'form_profile_error', 'conf_profile', 140, submit_flg, 6, 'register_input_conf_btn', 'submit_btn');
+    nullableImage('input_profile_image', 'form_profile_image_error', 'conf_image', submit_flg, 7, 'register_input_conf_btn', 'submit_btn');
+    
+    sectionChange('register_input', 'register_conf', 'register_input_conf_btn');
+    returnClick('register_input' ,'register_conf', 'form_return_btn');
+});
