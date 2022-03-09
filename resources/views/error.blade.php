ba1f5b08b12fb445a5ebc11544dc06e353b9b4de
oshiro-kazuki <ka.oo1213mi@gmail.com>
2022-03-10 06:53:47 +0900

    認証系追加

---
+++ resources/views/error.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('const.APP_NAME')}} エラー</title>
+
+        <!-- fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <!-- style -->
+        <link rel="stylesheet" href="{{ asset('css/common/reset.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/common/header.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/common/footer.css') }}">
+    </head>
+    <body>
+        @include('common.header')
+
+        <main>
+            <h6>エラー</h6>
+        </main>
+
+        @include('common.footer')
+
+        <script src="{{ asset('js/common/dom_operation.js') }}"></script>
+        <script src="{{ asset('js/common/header.js') }}"></script>
+        <script src="{{ asset('js/common/footer.js') }}"></script>
+    </body>
+</html>
