1723d55277e038eea07eb73e6dc04507e545df12
oshiro-kazuki <ka.oo1213mi@gmail.com>
2022-03-14 18:17:34 +0900

    ログイン画面スタイル修正

--- resources/views/auth/owner/login.blade.php
+++ resources/views/auth/owner/login.blade.php
@@ -26,8 +26,8 @@
             <button id="login_submit" class="form_submit_btn" type="submit" disabled>ログイン</button>
         </section>
     </form>
+    <section class="form_link">
+        <a href="/owner/register">新規登録はこちら</a>
+    </section>
 </article>
-<section class="form_link">
-    <a href="/owner/register">新規登録はこちら</a>
-</section>
 @endsection
