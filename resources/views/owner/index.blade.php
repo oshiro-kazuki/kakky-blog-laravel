7c319fe0041bb07b1b1400a5c181522c9a01a7c3
oshiro-kazuki <ka.oo1213mi@gmail.com>
2022-03-16 08:29:49 +0900

    オーナー画面スタイル修正

--- resources/views/owner/index.blade.php
+++ resources/views/owner/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{config('const.APP_NAME')}}</title>
+        <title>{{config('const.APP_NAME')}} オーナー画面</title>
 
         <!-- fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -17,17 +17,19 @@
         <link rel="stylesheet" href="{{ asset('css/owner/index.css') }}">
     </head>
     <body>
-        @include('common.header')
-
+        @include('include.common.header')
         <main>
             <h1>{{ $owner['name'] }}　様</h1>
+            <nav class="owner_link_area">
+                <ul>
+                    <li><a href="/owner/news_input" class="news_input_btn">ニュース入力画面</a></li>
+                    <li><a href="/owner/logout" class="logout_btn">ログアウト</a></li>
+                </ul>
+            </nav>
+            
+            @include('include.button.top_btn')
         </main>
-        <aside class="logout">
-            <a href="/owner/logout" class="logout_btn">ログアウト</a>
-        </aside>
-
-        @include('common.footer')
-
+        @include('include.common.footer')
         <script src="{{ asset('js/common/dom_operation.js') }}"></script>
         <script src="{{ asset('js/common/header.js') }}"></script>
         <script src="{{ asset('js/common/footer.js') }}"></script>
